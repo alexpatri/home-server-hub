856aa1bb7ea1876b237737018689ceb38e667323
Alexsandro Júnior <jalexsandro2005@gmail.com>
2025-07-02 20:00:48 -0300

    feat: criado projeto frontend base

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="h-screen w-screen flex items-center justify-center bg-dark">
+    <h1 class="text-light text-2xl">Hello, World!</h1>
+  </main>
+</template>
