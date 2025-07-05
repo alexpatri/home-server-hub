06752bf6f95100f84dd8c2054ab82c0814b993bb
Alexsandro Júnior <jalexsandro2005@gmail.com>
2025-07-04 22:42:46 -0300

    feat: carregamento de temperatura e localização dinâmico

--- frontend/src/components/Clock.vue
+++ frontend/src/components/Clock.vue
@@ -1,10 +1,10 @@
-<!-- components/Clock.vue -->
 <template>
-  <div class="flex flex-col items-center text-black dark:text-white">
+  <div class="flex flex-col items-start text-black dark:text-white gap-0.5">
     <h1 class="text-[64px] font-bold leading-none">{{ time }}</h1>
-    <div class="flex items-center space-x-2 mt-2 text-lg">
+
+    <div v-if="weather" class="flex items-center space-x-2 mt-2 text-lg">
       <font-awesome-icon :icon="['fas', 'cloud']" />
-      <span>23° Brasília, DF</span>
+      <span>{{ weather.temp }}° {{ weather.location }}</span>
     </div>
   </div>
 </template>
@@ -12,9 +12,10 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
+import axios from 'axios'
 
 const time = ref('00:00')
+const weather = ref<null | { temp: number; location: string }>(null)
 
 function updateTime() {
   const now = new Date()
@@ -23,9 +24,43 @@ function updateTime() {
   time.value = `${hours}:${minutes}`
 }
 
+async function fetchWeatherFromCoords(lat: number, lon: number) {
+  try {
+    // wttr.in aceita lat,lon como parâmetro de cidade
+    const res = await axios.get(`https://wttr.in/${lat},${lon}?format=j1`)
+
+    const city = res.data.nearest_area[0].areaName[0].value
+    const region = res.data.nearest_area[0].region[0].value
+    const temp = parseInt(res.data.current_condition[0].temp_C)
+
+    const location = `${city}, ${region}`
+
+    weather.value = {
+      temp: temp,
+      location: location,
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function fetchWeather() {
+  try {
+    // wttr.in aceita lat,lon como parâmetro de cidade
+    const locRes = await axios.get('https://ipwho.is/')
+    const { city, region, latitude, longitude } = locRes.data
+
+    console.log(city, region)
+    fetchWeatherFromCoords(latitude, longitude)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 onMounted(() => {
   updateTime()
   const interval = setInterval(updateTime, 1000)
+  fetchWeather()
   onUnmounted(() => clearInterval(interval))
 })
 </script>
