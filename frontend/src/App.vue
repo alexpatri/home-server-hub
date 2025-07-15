f772baeeb07c2c73c9d1f9e559025243ef20b945
Alexsandro Júnior <jalexsandro2005@gmail.com>
2025-07-14 21:20:10 -0300

    feat: criado componente global de loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Loader from '@/components/Loader.vue'
 </script>
 
 <template>
   <RouterView />
+  <Loader />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/Loader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    v-if="isLoading"
+    class="fixed inset-0 bg-dark/40 backdrop-blur-sm flex items-center justify-center z-50"
+  >
+    <div
+      class="animate-spin w-14 h-14 border-4 border-light border-t-transparent rounded-full"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useLoading } from '@/stores/loadingStore'
+
+const { isLoading } = useLoading()
+</script>
